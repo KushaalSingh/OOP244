c1e62967f586f07145a1b8fbee75e9868ab28fef
Kushaal Singh <kna28@myseneca.ca>
2024-01-13 09:54:36 -0500

    main.cpp functions have been implemented

--- workshops_2.0/WS01/LAB/w1p1.cpp
+++ workshops_2.0/WS01/LAB/w1p1.cpp
@@ -32,45 +32,6 @@ void labelLine(int n, const char* label);
 // Draws a line
 void line(int n);
 
-// SeneGraph program
-int main() {
-    int noOfSamples = 0;
-    int samples[MAX_NO_OF_SAMPLES] = { 0 };
-    bool done = false;
-    labelLine(33, "Welcome to SeneGraph");
-    while (!done) {
-        switch (menu(noOfSamples)) {
-        case 1:
-            cout << "Enter number of available samples: ";
-            noOfSamples = getInt(1, MAX_NO_OF_SAMPLES);
-            break;
-        case 2:
-            if (noOfSamples == 0) {
-                samplesFirst();
-            }
-            else {
-                cout << "Please enter the sample values: " << endl;
-                getSamples(samples, noOfSamples);
-            }
-            break;
-        case 3:
-            if (noOfSamples == 0) {
-                samplesFirst();
-            }
-            else if (samples[0] == 0) {
-                cout << "Samples missing. Please enter the sample values!" << endl;
-            }
-            else {
-                printGraph(samples, noOfSamples, " SeneGraph ");
-            }
-            break;
-        case 0:
-            cout << "Thanks for using SeneGraph" << endl;
-            done = true;
-        }
-    }
-    return 0;
-}
 
 
 // prints a graph comparing the sample values visually 
