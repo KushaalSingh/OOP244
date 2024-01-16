5d57a1bafcc8ed42a3cecf55797ae63b83c3a4e3
Kushaal Singh <kna28@myseneca.ca>
2024-01-16 14:40:28 -0500

    All files added, code not implemented

---
+++ workshops_2.0/WS01/DIY/StMark.h
@@ -0,0 +1,16 @@
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#ifndef STUDENT_MARK_H
+#define STUDENT_MARK_H
+
+namespace seneca {
+
+	typedef struct {
+		char name[21];
+		char surname[31];
+		int mark;
+	} StMark;
+
+}
+
+#endif
