3e504fa6e7a16bff079cb759152db2ade379e529
Kushaal Singh <kna28@myseneca.ca>
2024-01-18 21:28:04 -0500

    Workshop Updated

--- workshops_2.0/WS01/LAB/io.cpp
+++ workshops_2.0/WS01/LAB/io.cpp
@@ -1,7 +1,6 @@
  // I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #include "io.h"
 using namespace std;
-
 namespace seneca {
     void printInt(int value, int fieldWidth) {
         cout << value;
