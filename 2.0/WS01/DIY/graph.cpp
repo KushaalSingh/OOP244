57c31773475b88420b008009a9f7afc4e6f50d12
Kushaal Singh <kna28@myseneca.ca>
2024-01-23 19:17:56 -0500

    Two new function added to display graph

--- 2.0/WS01/DIY/graph.cpp
+++ 2.0/WS01/DIY/graph.cpp
@@ -1,12 +1,47 @@
 // I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #include "io.h"
+#include "StMark.h"
 #include "graph.h"
 using namespace std;
 
 namespace seneca {
     const int MAX_NO_OF_SAMPLES = 20;
     const int GRAPH_WIDTH = 65;
-    void printGraph(int samples[], int noOfSamples, const char* label) {
+
+    void printGraph(StMark* stu, int stuNum, const char* label) {
+        int category[10] = { 0 };
+        labelLine(79, label);
+
+        //cateogryWithMostStudents(stu, stuNum, category);
+        for (int i = 0; i < 9; i++) {
+            std::cout << category[i] << std::endl;
+        }
+    }
+
+    int cateogryWithMostStudents(StMark* stu, int stuNum, int* category) {
+        int i;
+       
+        for (i = 0; i < stuNum; i++) {
+            if (stu[i].mark >= 0 && stu[i].mark <= 10) category[0]++;
+            else if (stu[i].mark >= 11 && stu[i].mark <= 20) category[1]++;
+            else if (stu[i].mark >= 21 && stu[i].mark <= 30) category[2]++;
+            else if (stu[i].mark >= 31 && stu[i].mark <= 40) category[3]++;
+            else if (stu[i].mark >= 41 && stu[i].mark <= 50) category[4]++;
+            else if (stu[i].mark >= 51 && stu[i].mark <= 60) category[5]++;
+            else if (stu[i].mark >= 61 && stu[i].mark <= 70) category[6]++;
+            else if (stu[i].mark >= 71 && stu[i].mark <= 80) category[7]++;
+            else if (stu[i].mark >= 81 && stu[i].mark <= 90) category[8]++;
+            else if (stu[i].mark >= 91 && stu[i].mark <= 100) category[9]++;
+        }
+    }
+
+
+
+
+
+
+
+    void _printGraph(int samples[], int noOfSamples, const char* label) {
         int max = findMax(samples, noOfSamples);
         labelLine(GRAPH_WIDTH + 10, label);
         for (int i = 0; i < noOfSamples; i++) {
--- 2.0/WS01/DIY/graph.h
+++ 2.0/WS01/DIY/graph.h
@@ -3,10 +3,14 @@
 #define SENECA_GRAPH_H
 
 namespace seneca {
-	void printGraph(int samples[], int noOfSamples, const char* label);
+	void _printGraph(int samples[], int noOfSamples, const char* label);
 	void printBar(int val, int max);
 	void getSamples(int samples[], int noOfSamples);
 	int findMax(int samples[], int noOfSamples);
+
+	void printGraph(StMark* stu, int stuNum, const char* label);
+	int cateogryWithMostStudents(StMark* stu, int stuNum, int* category);
+
 }
 
 #endif
