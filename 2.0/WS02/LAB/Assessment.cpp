6e4496bd3a4d052960622587458a840c0dcd5bd9
Kushaal Singh <kna28@myseneca.ca>
2024-01-28 21:56:06 -0500

    Changed dynamic memory allocation line

--- 2.0/WS02/LAB/Assessment.cpp
+++ 2.0/WS02/LAB/Assessment.cpp
@@ -19,9 +19,10 @@ namespace seneca {
 		char tempTitle[STRING_LEN];
 
 		if (read(tempMark, fptr) && read(tempTitle, fptr)) {
-			assess.m_mark = new double[tempMark];
+			assess.m_mark = new double(tempMark);
 			assess.m_title = new char[STRING_LEN];
 			strcpy(assess.m_title, tempTitle);
+
 			return true;
 		}
 
