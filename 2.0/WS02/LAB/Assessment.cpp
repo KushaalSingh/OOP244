4b8fdb2d4257aba5a9aaca1e25deb6e370522e5c
Kushaal Singh <kna28@myseneca.ca>
2024-02-10 17:29:47 -0500

    Final read function created

--- 2.0/WS02/LAB/Assessment.cpp
+++ 2.0/WS02/LAB/Assessment.cpp
@@ -14,15 +14,39 @@ namespace seneca {
 		return fscanf(fptr, ",%60[^\n]\n", cstr);
 	}
 
-	bool read(Assessement& assess, FILE* fptr) {
+	bool read(Assessment& assess, FILE* fptr) {
 		double mk;
 		char str[STRING_LEN];
 		if (!read(mk, fptr) || !read(str, fptr)) return false;
 		assess.m_mark = new double;
-		*assess.m_mark = mk;
+		*(assess.m_mark) = mk;
 		assess.m_title = new char[strlen(str) + 1];
 		strcpy(assess.m_title, str);
 		return true;
 	}
 
+	void freeMem(Assessment*& aptr, int size) {
+		int i;
+		for (i = 0; i < size; i++) {
+			delete aptr[i].m_mark;
+			delete[] aptr[i].m_title;
+		}
+		delete[] aptr;
+		aptr = nullptr;
+	}
+
+	int read(Assessment*& aptr, FILE* fptr) {
+		int i, size, count = 0;
+		read(size, fptr);
+		aptr = new Assessment[size];
+		for (i = 0; i < size; i++) {
+			if (!read(aptr, fptr)) break;
+			++count;
+		}
+		if (size != count) {
+			delete[] aptr;
+			return 0;
+		}
+		return size;
+	}
 }
--- 2.0/WS02/LAB/Assessment.h
+++ 2.0/WS02/LAB/Assessment.h
@@ -13,12 +13,12 @@ namespace seneca {
 	typedef struct {
 		double* m_mark;
 		char* m_title;
-	} Assessement;
+	} Assessment;
 
 	bool read(int& value, FILE* fptr);
 	bool read(double& value, FILE* fptr);
 	bool read(char* cstr, FILE* fptr);
-	bool read(Assessement& assess, FILE* fptr);
+	bool read(Assessment& assess, FILE* fptr);
 	void freeMem(Assessment*& aptr, int size);
 	int read(Assessment*& aptr, FILE* fptr);
 
