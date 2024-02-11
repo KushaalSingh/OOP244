37b848589f1f76e26ddb56608d85aa3f69935641
Kushaal Singh <kna28@myseneca.ca>
2024-02-11 12:25:35 -0500

    Room datastructure added

---
+++ 2.0/WS02/DIY/Room.h
@@ -0,0 +1,15 @@
+#include "Guest.h"
+
+#ifndef SENECA_ROOM_H
+#define SENECA_ROOM_H
+
+namespace seneca {
+	
+	typedef struct {
+		char m_roomNumber[6];
+		int m_noOfGuests;
+		Guest* m_guests;
+	} Room;
+}
+
+#endif
