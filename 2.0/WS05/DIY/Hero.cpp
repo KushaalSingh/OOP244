fc95f7d72e657c6c6120046aff204e17f8b12e39
Kushaal Singh <kna28@myseneca.ca>
2024-03-09 02:19:23 -0500

    displayHeroInfo & opeartor+=  and updatePowerlvevl added

--- 2.0/WS05/DIY/Hero.cpp
+++ 2.0/WS05/DIY/Hero.cpp
@@ -3,19 +3,42 @@
 namespace seneca {
 
 	Hero::Hero() {
-		name[0] = '\0';
- 		numPowers = 0;
-		tier = 0;
-		power = nullptr;
+		setEmpty();
 	}
 
 	Hero::Hero(const char* _name, Power _power[], int powerCount) {
+		setEmpty();
 		if (_name != nullptr) strcpy(name, _name);
-		numPowers = powerCount;
-		power = new Power[numPowers];
-		for (int i = 0; i < numPowers; i++) {
+		power_count = powerCount;
+		power = new Power[power_count];
+		for (int i = 0; i < power_count; i++) power[i] = _power[i];
+		power_level = updatePowerLevel(power, power_count);
+	}
 
-		}
+	void Hero::setEmpty() {
+		name[0] = '\0';
+		power_count = 0;
+		power_level = 0;
+		power = nullptr;
+	}
+
+	std::ostream& Hero::displayHeroInfo(std::ostream& print) const {
+		print << "Name: " << name << NL;
+		print << "List of available powers:" << NL;
+		for (int i = 0; i < power_count; i++) print << "  " << power[i].checkName() << NL;
+		print << "Power Level:" << power_level;
+	}
+
+	Hero& Hero::operator+=(Power& pwr) {
+		power[power_count++] = pwr;
+		power_level = updatePowerLevel(power, power_count);
+		return *this;
+	}
+
+	int updatePowerLevel(Power* pwr, int power_count) {
+		int total_rarity = 0;
+		for (int i = 0; i < power_count; i++) total_rarity += pwr[i].checkRarity();
+		return total_rarity * power_count;
 	}
 
 }
--- 2.0/WS05/DIY/Hero.h
+++ 2.0/WS05/DIY/Hero.h
@@ -3,18 +3,26 @@
 
 #include "Power.h"
 
+#define NL std::endl
+
 namespace seneca {
 
 	class Hero {
 	private:
 		char name[MAX_NAME_LENGTH];
-		int numPowers;
-		int tier;
+		int power_count;
+		int power_level;
 		Power* power;
+		void setEmpty();
 	public:
 		Hero();
 		Hero(const char* name, Power _power[], int powerCount);
+		std::ostream& displayHeroInfo(std::ostream& print = std::cout) const;
+		Hero& operator+=(Power& pwr);
 	};
+
+	int updatePowerLevel(Power* pwr, int power_count);
+
 
 }
 
