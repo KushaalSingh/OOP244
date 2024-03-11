a94ff05c2cbcc2b4a57e357a58c00a58b07c7b5b
Kushaal Singh <kna28@myseneca.ca>
2024-03-11 17:26:26 -0400

    main module added and implemented

--- Project/Milestone 1/Utils.cpp
+++ Project/Milestone 1/Utils.cpp
@@ -1,8 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdlib>
-#include <ctime>
+/*
+Citation and Sources...
+Final Project Milestone 1
+Module: Utils
+Filename: Utils.cpp
+Version  1.0
+Author   Kushaal Singh
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/?/?  Preliminary release
+2023/?/?  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+OR
+-----------------------------------------------------------
+Write exactly which part of the code is given to you as help and
+who gave it to you, or from what source you acquired it.
+-----------------------------------------------------------
+*/
 #include "Utils.h"
 using namespace std;
+
 namespace seneca {
     // start provided code
     bool debug = false;
--- Project/Milestone 1/Utils.h
+++ Project/Milestone 1/Utils.h
@@ -1,7 +1,31 @@
+/* 
+Citation and Sources...
+Final Project Milestone 1
+Module: Utils
+Filename: Utils.h
+Version  1.0
+Author   Kushaal Singh
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/?/?  Preliminary release
+2023/?/?  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+*/
+
 #ifndef SENECA_UTILS_H_
 #define SENECA_UTILS_H_
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 namespace seneca {
+
     class Utils {
         /// <summary>
         /// Start point for the simulated time
@@ -29,8 +53,6 @@ namespace seneca {
         /// </summary>
         /// <returns>the current time in minutes</returns>
         int getTime(); // returns the time of day in minutes
-
-
     };
 
     /// <summary>
