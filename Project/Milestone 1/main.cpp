4dda8d71eebbcb7b6763874c430a2e3f86d732a2
Kushaal Singh <kna28@myseneca.ca>
2024-03-11 18:07:03 -0400

    Time module added

--- Project/Milestone 1/main.cpp
+++ Project/Milestone 1/main.cpp
@@ -1,4 +1,20 @@
-#include <iostream>
+/*
+Citation and Sources...
+Final Project Milestone 1
+Module: Utils
+Filename: Utils.h
+Version  1.0
+Author   Kushaal Singh
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/?/?  Preliminary release
+2023/?/?  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+*/
 #include "Menu.h"
 #include "Time.h"
 #include "Utils.h"
