c91c51eecd602d6b28ddaa0512b93d592e93245b
Kushaal Singh <kna28@myseneca.ca>
2024-03-11 18:51:05 -0400

    default constructor and reset added

--- Project/Milestone 1/Time.cpp
+++ Project/Milestone 1/Time.cpp
@@ -1,1 +1,18 @@
-#pragma once
+#include "Time.h"
+
+namespace seneca {
+
+	Time& Time::reset() {
+		minutes = U.getTime();
+		return *this;
+	}
+
+	Time::Time(unsigned int min) {
+		minutes = min;
+	}
+
+	std::ostream& Time::write(std::ostream& print = std::cout) {
+		uint32_t hours = minutes / 
+	}
+
+}
--- Project/Milestone 1/Time.h
+++ Project/Milestone 1/Time.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef SENECA_TIME_CPP
+#define SENECA_TIME_CPP
+
+#include "Utils.h"
+
+namespace seneca {
+
+	class Time {
+	private:
+		uint32_t minutes;
+		Time& reset();
+	public:
+		Time(unsigned int min = 0u);
+		std::ostream& write(std::ostream& print = std::cout);
+	};
+
+}
+
+#endif 
