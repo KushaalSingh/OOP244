38e5c74f2e19dd28074f291d945a537d6bbe7205
Kushaal Singh <kna28@myseneca.ca>
2024-03-16 23:32:43 -0400

    All functions implemented

--- 2.0/WS06/LAB/EmailFile.cpp
+++ 2.0/WS06/LAB/EmailFile.cpp
@@ -1,3 +1,4 @@
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #include "EmailFile.h"
 
 namespace seneca {
@@ -49,20 +50,17 @@ namespace seneca {
         delete[] m_email;
     }
 
-    void EmailFile::setFilename(const char* filename)
-    {
+    void EmailFile::setFilename(const char* filename) {
         delete[] m_filename;
         m_filename = nullptr;
-        if (filename)
-        {
+        if (filename) {
             m_filename = new char[strlen(filename) + 1];
             m_filename[0] = '\0';
             strcpy(m_filename, filename);
         }
     }
 
-    void EmailFile::setEmpty()
-    {
+    void EmailFile::setEmpty() {
         delete[] m_emailLines;
         delete[] m_filename;
         m_emailLines = nullptr;
@@ -73,21 +71,19 @@ namespace seneca {
     bool EmailFile::setNoOfEmails() {
 
         bool isValid = false;
-        ifstream file(m_filename);
+        std::ifstream file(m_filename);
 
         if (!file.is_open()) {
-            cout << "Failed to open file: " << m_filename << endl;
+            std::cout << "Failed to open file: " << m_filename << std::endl;
         }
-        else
-        {
+        else {
             m_noOfEmails = 0;
             while (file) {
                 m_noOfEmails += (file.get() == '\n');
             }
             file.close();
 
-            if (m_noOfEmails == 0)
-            {
+            if (m_noOfEmails == 0) {
                 delete[] m_filename;
                 m_filename = nullptr;
             }
@@ -99,30 +95,96 @@ namespace seneca {
         return isValid;
     }
 
-    std::ostream& EmailFile::view(std::ostream& ostr) const
-    {
+    std::ostream& EmailFile::view(std::ostream& ostr) const {
         if (m_filename) {
-            ostr << m_filename << endl;
+            ostr << m_filename << std::endl;
             ostr.width(strlen(m_filename));
             ostr.fill('=');
-            ostr << "=" << endl;
+            ostr << "=" << std::endl;
             ostr.fill(' ');
             for (int i = 0; i < m_noOfEmails; i++) {
                 ostr.width(35);
-                ostr.setf(ios::left);
+                ostr.setf(std::ios::left);
                 ostr << m_emailLines[i].m_email;
                 ostr.width(25);
                 ostr << m_emailLines[i].m_name;
-                ostr << "Year = " << m_emailLines[i].m_year << endl;
+                ostr << "Year = " << m_emailLines[i].m_year << std::endl;
             }
         }
-
         return ostr;
     }
 
-    std::ostream& operator<<(std::ostream& ostr, const EmailFile& text)
-    {
+    std::ostream& operator<<(std::ostream& ostr, const EmailFile& text) {
         return text.view(ostr);
     }
 
+    void EmailFile::loadEmails() {
+        if (!m_filename) return;
+
+        delete[] m_emailLines;
+        m_emailLines = nullptr;
+        m_emailLines = new Email[m_noOfEmails];
+        std::ifstream fistr(m_filename);
+        for (int i = 0; i < m_noOfEmails; i++) if (!m_emailLines[i].load(fistr))
+            std::cerr << "Error reading from the file." << std::endl;
+    }
+
+    EmailFile::operator bool() const {
+        return m_filename != nullptr;
+    }
+
+
+    EmailFile::EmailFile() {
+        setEmpty();
+    }
+
+    EmailFile::EmailFile(const char* filename) {
+        if (filename == nullptr) setEmpty();
+        setFilename(filename);
+        if (setNoOfEmails()) std::cout << "Error, could not set m_noOfEmails" << std::endl;
+        loadEmails();
+    }
+
+    EmailFile::EmailFile(const EmailFile& src) {
+        setEmpty();
+        if ((bool)src) *this = src;
+    }
+
+    EmailFile& EmailFile::operator=(const EmailFile& src) {
+        if (this != &src) {
+            setEmpty();
+            setFilename(src.m_filename);
+            m_noOfEmails = src.m_noOfEmails;
+            m_emailLines = new Email[m_noOfEmails];
+            for (int i = 0; i < m_noOfEmails; i++) m_emailLines[i] = src.m_emailLines[i];
+        }
+        return *this;
+    }
+
+    bool EmailFile::saveToFile(const char* filename) const {
+        std::ofstream fostr(filename);
+        if (!fostr.is_open()) {
+            std::cout << "Error: Could not open file: " << filename << std::endl;
+            return false;
+        }
+        for (int i = 0; i < m_noOfEmails; i++) fostr << m_emailLines[i].m_email << ", " << 
+            m_emailLines[i].m_name << ", " << m_emailLines[i].m_year << std::endl;
+        return true;
+    }
+
+    void EmailFile::fileCat(const EmailFile& obj, const char* name = nullptr) {
+        if (!*this || !obj) return;
+        int total_emails = m_noOfEmails + obj.m_noOfEmails;
+        Email* merged;
+        merged = new Email[total_emails];
+        for (int i = 0; i < m_noOfEmails; i++) merged[i] = m_emailLines[i];
+        for (int i = 0; i < obj.m_noOfEmails; i++) merged[m_noOfEmails + i] = obj.m_emailLines[i];
+        delete[] m_emailLines;
+        m_emailLines = merged;
+        m_noOfEmails = total_emails;
+        if (name) {
+            delete[] m_filename;
+            setFilename(name);
+        }
+    }
 }
--- 2.0/WS06/LAB/EmailFile.h
+++ 2.0/WS06/LAB/EmailFile.h
@@ -1,3 +1,4 @@
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #ifndef EMAIL_FILE_CPP
 #define EMAIL_FILE_CPP
 
@@ -16,15 +17,34 @@ namespace seneca {
 		char m_year[5]{ '\0' };
 		Email() {};
 		Email& operator=(const Email&);
-		Email(const Email& email);
+		Email(const Email& email) = delete;
 		bool load(std::ifstream& in);
 		~Email();
 		friend class EmailFile;
 	};
 
-	class EmailFile : private Email {
+	class EmailFile {
+	private:
+		Email* m_emailLines{ nullptr };
+		char* m_filename{};
+		int m_noOfEmails{ 0 };
+		void setFilename(const char* filename);
+		void setEmpty();
+		bool setNoOfEmails();
+		void loadEmails();
+		operator bool()const;
 
+	public:
+		EmailFile();
+		EmailFile(const char* filename);
+		EmailFile(const EmailFile& src);
+		EmailFile& operator=(const EmailFile& src);
+		std::ostream& view(std::ostream& ostr) const;
+		bool saveToFile(const char* filename) const;
+		void fileCat(const EmailFile& obj, const char* name = nullptr);
 	};
+
+	std::ostream& operator<<(std::ostream& ostr, const EmailFile& text);
 
 }
 
