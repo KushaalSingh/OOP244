80a29be402c533776155822cb498b8ddf3084910
Kushaal Singh <kna28@myseneca.ca>
2024-03-17 19:35:16 -0400

    Project Completed

--- Project/Milestone 2/IOAble.h
+++ Project/Milestone 2/IOAble.h
@@ -27,13 +27,11 @@ namespace seneca {
 	public:
 		virtual std::ostream& write(std::ostream& out) const = 0;
 		virtual std::istream& read(std::istream& in) = 0;
-		virtual ~IOAble() = 0;
+		virtual ~IOAble() {};
 	};
 
 	std::ostream& operator<<(std::ostream& out, const IOAble& src);
 	std::istream& operator>>(std::istream& in, IOAble& src);
-
-
 }
 
 
