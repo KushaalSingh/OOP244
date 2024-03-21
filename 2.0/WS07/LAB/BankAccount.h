23ee14f4b3bb822f561d68deda3338b5c5d3d3ca
Kushaal Singh <kna28@myseneca.ca>
2024-03-20 21:22:58 -0400

    Citation added

--- 2.0/WS07/LAB/BankAccount.h
+++ 2.0/WS07/LAB/BankAccount.h
@@ -1,3 +1,4 @@
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #ifndef SENECA_BANK_ACCOUNT_H
 #define SENECA_BANK_ACCOUNT_H
 
--- 2.0/WS07/LAB/DepositUtility.h
+++ 2.0/WS07/LAB/DepositUtility.h
@@ -1,3 +1,4 @@
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #ifndef SENECA_DEPOSIT_UTILITY_H
 #define SENECA_DEPOSIT_UTILITY_H
 
