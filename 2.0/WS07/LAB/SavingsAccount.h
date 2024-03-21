90d4193b0e08fb3547e4aa4ada9342e7d7e0c66a
Kushaal Singh <kna28@myseneca.ca>
2024-03-20 23:31:43 -0400

    SA added

---
+++ 2.0/WS07/LAB/SavingsAccount.h
@@ -0,0 +1,22 @@
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+#ifndef SENECA_SAVINGS_ACCOUNT_H
+#define SENECA_SAVINGS_ACCOUNT_H
+
+#include "BankAccount.h"
+
+namespace seneca {
+
+	class SavingsAccount : public BankAccount {
+	private:
+		double m_interest;
+		Date m_interestDate;
+		void writeInterest(std::ostream& out) const;
+		
+	public:
+		SavingsAccount(const char* name, Date openDate, DepositInfo depInfo, double balance, double interest, Date interestDate);
+		void applyInterest(Date& dt);
+	};
+
+}
+
+#endif
