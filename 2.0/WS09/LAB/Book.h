fa0a7e892c8a0d5dfb3da2f7726a57944dfb16fb
Kushaal Singh <kna28@myseneca.ca>
2024-04-01 20:14:02 -0400

    Book class added

--- 2.0/WS09/LAB/Book.h
+++ 2.0/WS09/LAB/Book.h
@@ -1,1 +1,23 @@
-#pragma once
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#ifndef SENECA_BOOK_H
+#define SENECA_BOOK_H
+
+#include "LibraryItem.h"
+
+namespace seneca {
+
+	class Book : public LibraryItem {
+	private:
+		char* m_author;
+
+	public:
+
+
+
+	};
+
+
+}
+
+#endif
